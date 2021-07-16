bff126ee09e11add815a047e3207ec2417b04249
Alex Bradley <36222451+alextbradley@users.noreply.github.com>
2021-07-16 07:37:50 +0100

    Delete MISMIP_ice2r_2km directory

--- MISMIP_ice2r_2km/gen_final_thickness_ice2r_2km.m
+++
@@ -1,7 +0,0 @@
-%Output a binary file with the thickness at the end of the ice2r_2km simulation (initial condition for ice2ra and ice2rr)
-
-fname = "/data/icesheet_output/aleey/wavi/MISMIP_019/run/outfile.nc";
-h = ncread(fname, 'h');
-h = h(:,:,end);
-fid=fopen('ice2r_2km_finalThickness.bin','w','b'); fwrite(fid,h,'real*8'); fclose(fid);
-
